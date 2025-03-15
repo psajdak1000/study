4d021044ee051bb00514a3cddb1a377d0d7f998b
Psajdak <psajdak1000@gmail.com>
2025-03-15 23:01:15 +0100

    lecim

---
+++ src/bootcamp/week5/finalKeyWord/Cabriolet.java
@@ -0,0 +1,30 @@
+package bootcamp.week5.finalKeyWord;
+
+public final class Cabriolet {
+    private final boolean roofopened;
+
+    private final String color;
+    public Cabriolet(boolean roofopened, String color) {
+        this.roofopened = roofopened;
+        this.color = color;
+    }
+
+    public boolean isRoofopened() {
+        return roofopened;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cabriolet{" +
+                "roofopened=" + roofopened +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public Cabriolet closeTheRoof() {
+        return new Cabriolet(false, this.color);
+        }
+}
--- src/bootcamp/week5/finalKeyWord/finalKeyword.java
+++ src/bootcamp/week5/finalKeyWord/finalKeyword.java
@@ -2,6 +2,12 @@ package bootcamp.week5.finalKeyWord;
 
 public class finalKeyword {
     public static void main(String[] args) {
-
+        Cabriolet cabriolet = new Cabriolet(true,"red");
+        cabriolet.getColor();
+        cabriolet.isRoofopened();
+        Cabriolet cabriolet1 = cabriolet.closeTheRoof();
+        System.out.println("cabrio " +cabriolet);
+        System.out.println("cabrio1 " +cabriolet1);
+        System.out.println(cabriolet==cabriolet1);
     }
 }
