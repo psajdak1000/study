c4398e6ffa8e2cb4d101416ac314af1251322648
Psajdak <psajdak1000@gmail.com>
2025-03-16 23:35:45 +0100

    lecim

---
+++ src/bootcamp/week5/finalKeyWord/Dog.java
@@ -0,0 +1,11 @@
+package bootcamp.week5.finalKeyWord;
+
+public class Dog {
+    private final String breed;
+    private final String name;
+
+    public Dog(String breed, String name) {
+        this.breed = breed;
+        this.name = name;
+    }
+}
