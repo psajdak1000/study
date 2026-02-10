3d1564516d2255fddfd9e29cb7e469d4160ffbe5
Psajdak <psajdak1000@gmail.com>
2026-02-10 19:54:06 +0100

    clean code

---
+++ src/JB/exercise/WithoutIf.java
@@ -0,0 +1,12 @@
+package JB.exercise;
+
+public class WithoutIf {
+    public static void main(String[] args) {
+        int ageMale = 25;
+        int ageFemale = 30;
+
+        System.out.println("Is ageMale greater than ageFemale? " + (ageMale > ageFemale));
+
+
+    }
+}
