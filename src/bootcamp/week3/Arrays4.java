fd6522a7ea46e8d5acc1bb1622584c40774e0129
Psajdak <psajdak1000@gmail.com>
2026-03-08 22:39:58 +0100

    arrays4

---
+++ src/bootcamp/week3/Arrays4.java
@@ -0,0 +1,23 @@
+package bootcamp.week3;
+
+import java.util.Arrays;
+
+public class Arrays4 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(array));
+        printArrayElements(array);
+    }
+
+    private static void printArrayElements(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
+}
