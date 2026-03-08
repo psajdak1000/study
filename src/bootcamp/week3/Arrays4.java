7385c8ae7e71f712dd98ff693ce53c37f03347e1
Psajdak <psajdak1000@gmail.com>
2026-03-08 22:59:48 +0100

    arrays4

--- src/bootcamp/week3/Arrays4.java
+++ src/bootcamp/week3/Arrays4.java
@@ -7,6 +7,27 @@ public class Arrays4 {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(Arrays.toString(array));
         printArrayElements(array);
+        getMaxValue(array);
+        getAvgValue(array);
+    }
+
+    private static void getAvgValue(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        double avg = (double) sum / array.length;
+        System.out.println("\nAverage value in the array is: " + avg);
+    }
+
+    private static void getMaxValue(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println("\nMax value in the array is: " + max);
     }
 
     private static void printArrayElements(int[] array) {
@@ -19,5 +40,7 @@ public class Arrays4 {
         }
         System.out.print("]");
     }
+
+
 
 }
